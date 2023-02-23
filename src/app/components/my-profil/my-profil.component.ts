558d2da143c93f2934e064b2a3533d3b7f732b94
camillebib <camille.bibaut0@gmaiL.com>
2023-02-23 13:56:53 +0100

    Tests prompt

--- src/app/components/my-profil/my-profil.component.ts
+++ src/app/components/my-profil/my-profil.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import { DialogService } from 'src/app/services/dialog.service';
 import { UserService } from 'src/app/services/user.service';
 import {User} from "../../models/user";
 
 @Component({
   selector: 'app-my-profil',
   templateUrl: './my-profil.component.html',
-  styleUrls: ['./my-profil.component.css']
+  styleUrls: ['./my-profil.component.css'],
 })
 export class MyProfilComponent implements OnInit {
 
@@ -20,17 +22,51 @@ export class MyProfilComponent implements OnInit {
     password: ''
   };
 
+  @ViewChild("dialogLog")
+  dialogLog!: ElementRef;
+
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private dialogService: DialogService
   ) { }
 
   ngOnInit(): void {
     this.getUser;
   }
 
+  showClickUpdateUsername() {
+    this.dialogService.showDialog(this.dialogLog.nativeElement, {
+      content: `<table style="border-collapse: separate;border-spacing: 10px;width:85%;margin: 0px -5px 0px;">
+                <tr>
+                    <td>Modifier votre pseudo:</td>
+                </tr>
+                <tr>
+                    <td><span class="e-input-group">
+                    <input type="text" id="username" (focus)='onFocus()' (blur)='onBlur()' name="Required" class="e-input" />
+                    </span></td>
+                </tr>
+            </table>`,
+            buttons:  [{ click: this.hideClick.bind(this), buttonModel: { content: 'Valider', isPrimary: true } }, { click: this.hideClick.bind(this), buttonModel: { content: 'Annuler' } }]
+    });
+  }
+ 
+  hideClick() {
+   this.dialogService.hideDialog();
+  }
+
   getUser():void{
     this.userService.fetchUser()
     .subscribe((user: User[]) => this.user = user);
+  }
+
+  updateUsername(){
+    let saisie = prompt("Nouveau nom d'utilisateur :");
+        if (saisie == null || saisie == "") {
+            prompt("Modification annulée");
+        } else {
+            //TODO: Call update method
+            prompt("Nom d'utilisateur mis à jour");
+        } 
   }
 
 }
